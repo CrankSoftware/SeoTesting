fc2c24db842be81df653196418cd03e31b3d36b5
DESKTOP-35AUFDF\marcs <marcsstevenson@hotmail.com>
2018-01-24 12:54:13 +1300

    Added pages to navigate to

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HowComponent } from './how/how.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'About',
+    component: AboutComponent
+  },
+  {
+    path: 'How',
+    component: HowComponent
+  },
+  {
+    path: 'Contact',
+    component: ContactComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { AboutComponent } from './about/about.component';
+import { HowComponent } from './how/how.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    AboutComponent,
+    HowComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
